63d1e16d9d9abf3ec464006109ed3ee588740139
Godly T.Alias <godlytalias@yahoo.co.in>
2020-10-17 19:39:19 +0530

    Added comments
    Signed-off-by: Godly T.Alias <godlytalias@yahoo.co.in>

--- GospelTranslator.php
+++ GospelTranslator.php
@@ -11,6 +11,7 @@ class GospelTranslator {
         $this->db->dbConnect();
     }
 
+    //get the role of a user
     function getRole($userID) {
         $result = $this->db->dbQuery('Select roleID from User where userID=' . $userID);
         if ($result->num_rows > 0) {
@@ -19,6 +20,7 @@ class GospelTranslator {
         } else return -1;
     }
 
+    //get the role name of a role id
     function getRoleName($roleID) {
         $result = $this->db->dbQuery('Select roleName from role where roleID=' . $roleID);
         if ($result->num_rows > 0) {
@@ -27,6 +29,7 @@ class GospelTranslator {
         } else return -1;
     }
 
+    //function to add a user to system
     function addUser($table, $phone, $name, $roleID, $password, $email) {
         $userValues = array('phoneNumber' => $phone,
                         'name' => $name,
@@ -38,6 +41,7 @@ class GospelTranslator {
         return $userID;
     }
 
+    //function to get user profile of a user
     function getUser($phone) {
         $userFetch = $this->db->dbQuery('select * from User where phoneNumber=' . $phone);
         if ($userFetch->num_rows > 0) {
@@ -46,6 +50,7 @@ class GospelTranslator {
         } else return -1;
     }
 
+    //function to authenticate a user
     function canAllowLogin($phone, $password) {
         $user = $this->getUser($phone);
         if (is_numeric($user) && $user == -1) return false;
