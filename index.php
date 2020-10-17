5141b0c3e16b2168e4e8b71d9a3e71a020ddd038
Godly T.Alias <godlytalias@yahoo.co.in>
2020-10-17 21:52:51 +0530

    Add sample for translation need querying
    Signed-off-by: Godly T.Alias <godlytalias@yahoo.co.in>

--- index.php
+++ index.php
@@ -5,20 +5,24 @@ include 'GospelTranslator.php';
 
 $db = new GospelTranslator();
 
+//getting role name
 $result = $db->getRoleName(1);
 echo $result;
 echo '<br/>';
 
+//adding user
 $result = $db->addUser(7293333328472, 'godly', 1, 'testpassword', 'sldkjfs');
 echo $result;
 echo '<br/>';
 
+//checking credentials
 $result = $db->canAllowLogin(7293333328472, 'testpassword');
 if ($result)
 echo '<br/>login';
 else
 echo '<br/>block';
 
+//adding need
 $result = $db->addTransReq(1, 1, 1, "godly t alias", "my comments");
 echo '<br/>translation request added';
 
@@ -26,9 +30,18 @@ echo '<br/>';
 
 
 echo '<br/>';
+
+//getting all languages
 $langs = $db->getLang();
 foreach($langs as $lang){
 echo "find";
 echo $lang['langID'] . ' - ' . $lang['languageName'];
+}
+
+//getting trans needs for a translator
+$reqs = $db->getTransReq(1);
+foreach($reqs as $req) {
+echo $req['sourceText'];
+echo '<br/>';
 }
 ?>
