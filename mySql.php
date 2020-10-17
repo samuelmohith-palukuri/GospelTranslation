0704d6c521a8c1e57dbd310c9af55bfb15dfc082
Godly T.Alias <godlytalias@yahoo.co.in>
2020-10-17 18:07:54 +0530

    Updated mysql driver
    Signed-off-by: Godly T.Alias <godlytalias@yahoo.co.in>

--- mySql.php
+++ mySql.php
@@ -2,14 +2,14 @@
 
 include 'dbConfig.php';
 
-class Mysql extends Dbconfig {
+class mySql extends Dbconfig {
 
     public $dbConnection;
     public $dataSet;
     private $sqlQuery;
 
     protected $databaseName;
-    protected $hostName;
+    protected $serverName;
     protected $userName;
     protected $passCode;
 
@@ -20,24 +20,24 @@ class Mysql extends Dbconfig {
 
         $dbConf = new Dbconfig();
         $this->databaseName = $dbConf->dbName;
-        $this->hostName = $dbConf->serverName;
+        $this->serverName = $dbConf->serverName;
         $this->userName = $dbConf->userName;
         $this->passCode = $dbConf->passCode;
-        $dbConf = NULL;
     }
 
     public function dbConnect()    {
-        $this->dbConnection = mysql_connect($this->serverName,$this->userName,$this->passCode);
-        mysql_select_db($this->databaseName,$this->dbConnection);
+        $this->dbConnection = new mysqli($this->serverName, $this->userName, $this->passCode, $this->databaseName);
+        if (!$this->dbConnection)
+            die('Could not connect to Database');
         return $this->dbConnection;
     }
 
     public function dbQuery($query) {
-        return mysql_query($this->dbConnection, $query);
+        return $this->dbConnection->query($query);
     }
 
     public function dbClose() {
-        mysql_close($this->dbConnection);
+        $this->dbConnection->close();
     }
-
+}
 ?>
